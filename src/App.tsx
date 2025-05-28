254e570f67cb38deb42b7ab10f785cb758ded802
kunalrbhatia <kunal.bhatia@hotmail.com>
2025-05-29 02:29:01 +0530

    Implement style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import StoryDisplay from './components/StoryDisplay';
 import Controls from './components/Controls';
 import './index.css';
@@ -15,15 +15,11 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-emerald-500 min-h-screen flex flex-col items-center p-6">
-      <div className="min-h-fit bg-black text-white flex flex-col items-center justify-start p-6 shadow-lg rounded-md w-full">
-
-        <Controls
-          stories={stories}
-          onStorySelect={setSelectedStory}
-          fontSize={fontSize}
-          setFontSize={setFontSize}
-        />
+    <div className='bg-emerald-500 min-h-screen flex flex-col items-center p-6' style={{ backgroundImage: 'url(./src/assets/trees.png)', backgroundSize: 'cover' }}>
+      <h1 className='text-3xl font-bold mb-4 text-green-900 bg-green-500 bg-opacity-50 backdrop-blur-md rounded-md p-2'>Nature Theme Vision Therapy</h1>
+      <hr className='w-full mb-6 border-t-4 border-green-600 ' />
+      <div className='min-h-fit bg-black text-white flex flex-col items-center justify-start p-6 shadow-lg rounded-md w-full'>
+        <Controls stories={stories} onStorySelect={setSelectedStory} fontSize={fontSize} setFontSize={setFontSize} />
         <StoryDisplay story={selectedStory ? { content: selectedStory.content } : null} fontSize={fontSize} />
       </div>
     </div>
